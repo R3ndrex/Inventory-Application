bee98be76a4ed6a5022e2b7af0ae492bcdd61de3
R3ndrex <yacholohchtoli@gmail.com>
2026-03-11 16:15:35 +0200

    add deletem get games

--- db/queries.js
+++ db/queries.js
@@ -1,16 +1,28 @@
 const db = require("./pool");
+
 async function getCategories() {
     const { rows } = await db.query(`SELECT * FROM categories`);
     return rows;
 }
+
 async function deleteCategory(id) {
     await db.query("DELETE FROM categories WHERE id = $1", [id]);
+}
+async function deleteGame(id) {
+    await db.query("DELETE FROM games WHERE id = $1", [id]);
 }
 async function postCategory() {}
 async function updateCategory() {}
 
-async function getGames() {}
-async function deleteGame() {}
+async function getGames() {
+    const { rows } = await db.query(
+        `SELECT games.* FROM games 
+        LEFT JOIN categories 
+        ON games.categoryId = categories.id`,
+    );
+    return rows;
+}
+
 async function postGame() {}
 async function updateGame() {}
 module.exports = {
