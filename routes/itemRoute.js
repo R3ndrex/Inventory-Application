e18d0e299be5e75aeac55c09408afc06cdfb0940
R3ndrex <yacholohchtoli@gmail.com>
2026-03-11 17:33:18 +0200

    change order of routes to make add route possible

--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -2,11 +2,12 @@ const { Router } = require("express");
 const itemController = require("../controllers/itemController");
 const itemRouter = Router();
 
-itemRouter.route("/:category").get(itemController.getGame);
-
-itemRouter.route("/:game").post(itemController.postGame);
+itemRouter
+    .route("/add")
+    .get(itemController.getFormGame)
+    .post(itemController.postGame);
 
 itemRouter.get("/:game/delete", itemController.deleteGame);
 itemRouter.post("/:game/update", itemController.updateGame);
-
+itemRouter.get("/:category", itemController.getGame);
 module.exports = itemRouter;
