b3941d820600ed696d190b5a7ff63174cb9d76ed
R3ndrex <yacholohchtoli@gmail.com>
2026-03-11 14:16:53 +0200

    add validator, delete and getindex

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,25 @@
+const db = require("../db/queries");
+const { body, matchingData } = require("express-validator");
+
+const categoryValidator = [
+    body("category", "Format isn't correct ")
+        .trim()
+        .notEmpty()
+        .withMessage("Category can't be empty"),
+];
+
 module.exports = {
-    getIndex: (req, res) => {
-        return res.render("pages/index");
+    getIndex: async (_, res) => {
+        const categories = await db.getCategories();
+        return res.render("pages/index", { categories });
     },
     getCategory: () => {},
     postCategory: () => {},
-    deleteCategory: () => {},
+    deleteCategory: async (req, res) => {
+        const { category } = req.params;
+        console.log(category);
+        await db.deleteCategory(category);
+        res.redirect("/");
+    },
     updateCategory: () => {},
 };
