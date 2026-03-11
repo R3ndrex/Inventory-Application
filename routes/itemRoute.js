49257ab897ad198c7e8083437b0dbc2a9ec62215
R3ndrex <yacholohchtoli@gmail.com>
2026-03-11 16:17:36 +0200

    change delete route, getgame route

--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -2,12 +2,11 @@ const { Router } = require("express");
 const itemController = require("../controllers/itemController");
 const itemRouter = Router();
 
-itemRouter
-    .route("/:game")
-    .get(itemController.getGame)
-    .post(itemController.postGame)
-    .delete(itemController.deleteGame);
+itemRouter.route("/:category").get(itemController.getGame);
 
+itemRouter.route("/:game").post(itemController.postGame);
+
+itemRouter.get("/:game/delete", itemController.deleteGame);
 itemRouter.post("/:game/update", itemController.updateGame);
 
 module.exports = itemRouter;
