288479cd74b143adea35f39c29d0d29e581adce7
R3ndrex <yacholohchtoli@gmail.com>
2026-03-18 12:05:54 +0200

    add more game body validation, add getform for updating game

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -2,51 +2,97 @@ const db = require("../db/queries");
 const { body, matchedData, validationResult } = require("express-validator");
 
 const gameValidator = [
-    body("name", "Format isn't correct ")
+    body("name", "Format isn't correct")
         .trim()
         .notEmpty()
-        .withMessage("Name can't be empty"),
+        .withMessage("Name can't be empty")
+        .bail()
+        .custom(async (value) => {
+            const id = req?.params?.id;
+            const game = await db.findGameByName(value);
+            if (game && game.name === String(value) && id !== game.id) {
+                throw new Error("Name must be unique");
+            }
+            return true;
+        })
+        .bail(),
+    body("image", "Format isn't correct")
+        .trim()
+        .notEmpty()
+        .withMessage("Image Path can't be empty")
+        .bail(),
+    body("categories", "Format isn't correct")
+        .isArray({ min: 1 })
+        .withMessage("There must be 1 or more categories"),
+    body("developers", "Format isn't correct")
+        .isArray({ min: 1 })
+        .withMessage("There must be 1 or more deveopers"),
 ];
 
 module.exports = {
     getGame: async (req, res) => {
         const { gameId } = req.params;
-        const { name, developer, categories, imagesrc } =
+        const { name, developers, categories, imagesrc } =
             await db.getFullGameInfo(gameId);
         return res.render("pages/gamePage", {
             name,
-            developer,
+            developers,
             categories,
             imagesrc,
         });
     },
-    postGame: [
+    addGame: [
         gameValidator,
         async (req, res) => {
             const result = validationResult(req);
             if (!result.isEmpty()) {
-                const categories = await db.getCategories();
+                const allCategories = await db.getCategories();
+                const allDevelopers = await db.getDevelopers();
+                const { image, name, categories, developers } = req.body;
                 return res.status(400).render("pages/addGame", {
                     errors: result.array(),
+                    allDevelopers,
+                    allCategories,
                     categories,
+                    developers,
+                    image,
+                    name,
                 });
             }
-            const { name } = matchedData(req);
-            const { category, image } = req.body;
-
-            await db.postGame({ category, name, image });
+            const { name, image, categories, developers } = matchedData(req);
+            await db.postGame({
+                name,
+                image,
+                developers,
+                categories,
+            });
             return res.redirect("/");
         },
     ],
     deleteGame: async (req, res) => {
-        const { game } = req.params;
-        await db.deleteGame(game);
+        const { gameId } = req.params;
+        await db.deleteGame(gameId);
         return res.redirect("/");
     },
-    updateGame: () => {},
-    getFormGame: async (_, res) => {
-        const categories = await db.getCategories();
-        const developers = await db.getDevelopers();
-        return res.render("pages/addGame", { categories, developers });
+    getUpdateGameForm: async (req, res) => {
+        const { gameId } = req.params;
+        const { name, developers, categories, imagesrc } =
+            await db.getFullGameInfo(gameId);
+        const allCategories = await db.getCategories();
+        const allDevelopers = await db.getDevelopers();
+        return res.render("pages/updateGame", {
+            allCategories,
+            allDevelopers,
+            name,
+            developers,
+            categories,
+            image: imagesrc,
+        });
     },
+    getFormGame: async (_, res) => {
+        const allCategories = await db.getCategories();
+        const allDevelopers = await db.getDevelopers();
+        return res.render("pages/addGame", { allCategories, allDevelopers });
+    },
+    updateGame: (req, res) => {},
 };
