7087008f33f616398ad5801c727ac32d07f56a14
R3ndrex <yacholohchtoli@gmail.com>
2026-03-18 12:03:15 +0200

    add get form route, change update to edit

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,13 +4,13 @@ const categoryController = require("../controllers/categoryController");
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.getCategoryList);
-
+categoryRouter.get("/add", categoryController.getFormCategory);
 categoryRouter
     .route("/:category")
     .get(categoryController.getCategory)
     .post(categoryController.postCategory);
 
 categoryRouter.get("/:category/delete", categoryController.deleteCategory);
-categoryRouter.post("/:category/update", categoryController.updateCategory);
+categoryRouter.post("/:category/edit", categoryController.updateCategory);
 
 module.exports = categoryRouter;
