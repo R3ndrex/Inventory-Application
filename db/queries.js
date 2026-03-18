257609a0f2f493276f520c5396249f17c462cb74
R3ndrex <yacholohchtoli@gmail.com>
2026-03-18 12:01:59 +0200

    change getfullgameinfo, postgame to incluide games_developers, games_categories, change getgames to  getgamesbycategory

--- db/queries.js
+++ db/queries.js
@@ -19,7 +19,7 @@ async function getDevelopers() {
     return rows;
 }
 
-async function getGames(category) {
+async function getGamesByCategory(category) {
     const { rows } = await db.query(
         `SELECT games.* FROM games 
         LEFT JOIN games_categories
@@ -30,29 +30,57 @@ async function getGames(category) {
     );
     return rows;
 }
-async function postGame({ category, name, image }) {
-    await db.query(
-        `INSERT INTO games (name, imageSrc, categoryId) VALUES ($1,$2,$3);`,
-        [name, image, category],
-    );
+async function postGame({ categories, name, image, developers }) {
+    await db.query("BEGIN");
+    try {
+        const { rows } = await db.query(
+            `INSERT INTO games (name, imageSrc) VALUES ($1,$2) RETURNING id;`,
+            [name, image],
+        );
+        const id = rows[0].id;
+        for (const category of categories) {
+            await db.query(
+                `INSERT INTO games_categories (game_id,category_id) VALUES ($1,$2)`,
+                [id, category],
+            );
+        }
+        for (const developer of developers) {
+            await db.query(
+                `INSERT INTO games_developers (game_id,developer_id) VALUES ($1,$2)`,
+                [id, developer],
+            );
+        }
+
+        await db.query("COMMIT");
+    } catch (error) {
+        await db.query("ROLLBACK");
+        throw error;
+    }
+}
+async function findGameByName(name) {
+    const { rows } = await db.query(`SELECT * FROM games WHERE games.name=$1`, [
+        name,
+    ]);
+    return rows[0];
 }
 
 async function getFullGameInfo(gameId) {
     const { rows } = await db.query(
         `
-        SELECT games.name AS name,games.imageSrc as imagesrc, developers.name AS developer ,array_agg(categories.name) AS categories FROM games 
+        SELECT games.name AS name,games.imageSrc as imagesrc, array_agg( DISTINCT developers.name) AS developers, array_agg( DISTINCT categories.name) AS categories FROM games 
         LEFT JOIN games_categories 
         ON games_categories.game_id=games.id
         LEFT JOIN categories
         ON games_categories.category_id = categories.id
+        LEFT JOIN games_developers
+        ON games_developers.game_id=games.id 
         LEFT JOIN developers 
-        ON developers.game_id=games.id
+        ON games_developers.developer_id=developers.id
         WHERE games.id = $1
-        GROUP BY games.name,developers.name,games.imageSrc
+        GROUP BY games.name,games.imageSrc
         `,
         [gameId],
     );
-    console.log(rows);
     return rows[0];
 }
 
@@ -62,10 +90,11 @@ module.exports = {
     deleteCategory,
     postCategory,
     updateCategory,
-    getGames,
+    getGamesByCategory,
     deleteGame,
     postGame,
     updateGame,
     getDevelopers,
     getFullGameInfo,
+    findGameByName,
 };
