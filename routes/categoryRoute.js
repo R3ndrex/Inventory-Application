23dda5ab77b20aeeb2f247fa2a9766343c400aca
R3ndrex <yacholohchtoli@gmail.com>
2026-03-21 14:27:36 +0200

    add route for creating category and form

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,11 +4,12 @@ const categoryController = require("../controllers/categoryController");
 const categoryRouter = Router();
 
 categoryRouter.get("/", categoryController.getCategoryList);
-categoryRouter.get("/add", categoryController.getFormCategory);
 categoryRouter
-    .route("/:category")
-    .get(categoryController.getCategory)
-    .post(categoryController.postCategory);
+    .route("/add")
+    .get(categoryController.getFormCategory)
+    .post(categoryController.addCategory);
+
+categoryRouter.route("/:category").get(categoryController.getCategory);
 
 categoryRouter.get("/:category/delete", categoryController.deleteCategory);
 categoryRouter.post("/:category/edit", categoryController.updateCategory);
