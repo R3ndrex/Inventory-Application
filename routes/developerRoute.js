e405538d3520d73e199b21f5bc0b71262d0a5f7a
R3ndrex <yacholohchtoli@gmail.com>
2026-03-23 15:34:29 +0200

    add developer edit and index route

--- routes/developerRoute.js
+++ routes/developerRoute.js
@@ -3,10 +3,17 @@ const developerController = require("../controllers/developerController");
 
 const developerRouter = Router();
 
+developerRouter.get("/", developerController.getDevelopersList);
 developerRouter
     .route("/add")
     .get(developerController.getFormDeveloper)
     .post(developerController.addDeveloper);
 
 developerRouter.get("/:developer", developerController.getDeveloperGamesList);
+developerRouter.get("/:developer/delete", developerController.deleteDeveloper);
+developerRouter
+    .route("/:developer/edit")
+    .post(developerController.updateDeveloper)
+    .get(developerController.getUpdateForm);
+
 module.exports = developerRouter;
